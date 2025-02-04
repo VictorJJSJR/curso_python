75cb4579c56d435683226204852e20591cc2cc90
VictorJJSJR <megalovania14@gmail.com>
2025-02-04 10:35:12 -0700

    Se agrego las condiciones para ganar y las combinaciones posibles para ello

--- Linea/Gato/tablero.py
+++ Linea/Gato/tablero.py
@@ -1,6 +1,3 @@
-'''
-tablero.py: Dibuja el tablero del juego del gato.
-'''
 import random
 
 def dibujar_tablero(simbolos: dict):
@@ -8,7 +5,6 @@ def dibujar_tablero(simbolos: dict):
     Dibuja el tablero del juego del gato.
     '''
     print(f'''
-    
     {simbolos['1']} | {simbolos['2']} | {simbolos['3']}
     ---------
     {simbolos['4']} | {simbolos['5']} | {simbolos['6']}
@@ -19,7 +15,7 @@ def dibujar_tablero(simbolos: dict):
 def ia(simbolos: dict):
     '''Juega Maquina'''
     ocupado = True
-    while ocupado == True:
+    while ocupado:
         x = random.choice(list(simbolos.keys()))
         if simbolos[x] not in ['X','O']:
             simbolos[x] = 'O'
@@ -29,38 +25,43 @@ def usuario(simbolos: dict):
     '''Juega usuario'''
     lista_numeros = [str(i) for i in range(1, 10)]
     ocupado = True
-    while ocupado == True:
-     x = input('Ingresa el numero de la casilla: ')
-    if x in lista_numeros:
-        if simbolos[x] not in ['X','O']:
-            simbolos[x] = 'X'
-            ocupado = False
+    while ocupado:
+        x = input('Ingresa el numero de la casilla: ')
+        if x in lista_numeros:
+            if simbolos[x] not in ['X','O']:
+                simbolos[x] = 'X'
+                ocupado = False
+            else:
+                print('Casilla ocupada')
         else:
-            print('Casilla ocupada')
-    else:
-        print('Numero no valido')
+            print('Numero no valido')
 
+def condiciones_winner(simbolos: dict):
+    '''Verifica si hay un ganador'''
+    combinaciones = [
+        ('1', '2', '3'), ('4', '5', '6'), ('7', '8', '9'), # Filas
+        ('1', '4', '7'), ('2', '5', '8'), ('3', '6', '9'), # Columnas
+        ('1', '5', '9'), ('3', '5', '7') # Diagonales
+    ]
+    
+    for c in combinaciones:
+        if simbolos[c[0]] == simbolos[c[1]] == simbolos[c[2]]:
+            return simbolos[c[0]] # Devuelve 'X' o 'O' si hay ganador
+    
+    return None
 
 if __name__ == '__main__':
-    # Crear el diccionario con claves tipo string
-    numeros = [ str(i) for i in range(1, 10)]
     simbolos = {str(x): str(x) for x in range(1, 10)}
+    ganador = None
+    
+    while not ganador:
+        dibujar_tablero(simbolos)
+        usuario(simbolos)
+        ganador = condiciones_winner(simbolos)
+        if ganador:
+            break
+        ia(simbolos)
+        ganador = condiciones_winner(simbolos)
     
     dibujar_tablero(simbolos)
-    ia(simbolos)
-    #x = random.choice(numeros)
-    #numeros.remove(x)
-    #simbolos[x] = 'X'
-    dibujar_tablero(simbolos)
-    usuario(simbolos)
-    #o = random.choice(numeros)
-    #numeros.remove(o)
-    #simbolos[o] = 'O'
-    dibujar_tablero(simbolos)
-    #print(numeros)
-    # Modificar el diccionario correctamente
-    #simbolos['1'] = 'X'
-    #dibujar_tablero(simbolos)
-
-    #simbolos['5'] = 'O'
-    #dibujar_tablero(simbolos)
+    print(f'¡El ganador es {ganador}!')
