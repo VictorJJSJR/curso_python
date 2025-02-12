46982514025323b18f5eb8a559613fe2102dcbca
VictorJJSJR <131430262+VictorJJSJR@users.noreply.github.com>
2025-02-12 10:29:25 -0700

    (Se borro el anterior, asi que todo esta junto)
    Se agrego: convertir a minuscula y remover caracteres no deseados.

---
+++ Linea/ahorcado/funciones.py
@@ -0,0 +1,54 @@
+import string
+import unicodedata
+'''
+funciones auxiliares del juego Ahorcado
+'''
+
+def carga_archivo_texto(archivo:str)->list:
+    '''
+    Carga un archivo de texto y devuelve una lista con las oraciones del archivo
+    '''
+    with open(archivo, 'r',encoding='utf-8') as file:
+        oraciones = file.readlines()
+    return oraciones
+
+def carga_plantillas(nombre_plantilla:str)->dict:
+    '''
+    Carga las plantillas del juego apartir de un archivo de texto
+    '''
+    plantillas = {}
+    for i in range(6):
+        plantillas[i] = carga_archivo_texto(f'./plantillas/{nombre_plantilla}-{i}.txt')
+    return plantillas
+
+def despliega_plantilla(diccionario:dict, nivel:int):
+    '''
+    Despliega una plantilla del juego
+    '''
+    if nivel in diccionario:
+        template = diccionario[nivel]
+        for renglon in template:
+            print(renglon)
+
+def obten_palabras(lista:list)->list:
+    '''
+    Obtiene las palabras de un texto
+    '''
+    texto = ' '.join(lista[120:])
+    palabras = texto.split()
+    #Convertimos a minusculas
+    minusculas = [palabra.lower() for palabra in palabras]
+    set_palabras = set(minusculas)
+    #Removemos signos de puntuacion y caracteres especiales
+    set_palabras = {palabra.strip(string.punctuation) for palabra in set_palabras}
+    set_palabras = {palabra for palabra in set_palabras if palabra.isalpha()}
+    #Removeos acentos
+    set_palabras = {unicodedata.normalize('NFKD', palabra).encode('ASCII', 'ignore').decode('utf-8') for palabra in set_palabras}
+    return list(set_palabras)
+
+if __name__ == '__main__':
+    plantillas = carga_plantillas('plantilla')
+    despliega_plantilla(plantillas, 5)
+    lista_oraciones = carga_archivo_texto('./datos/pg15532.txt')
+    lista_palabras = obten_palabras(lista_oraciones)
+    print(lista_palabras[:50])
