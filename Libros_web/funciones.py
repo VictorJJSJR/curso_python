3cc966700e03bebea520e1657c464eae8ca22122
VictorJJSJR <megalovania14@gmail.com>
2025-03-06 10:46:19 -0700

    Primeras funciones de funciones.py

--- Libros_web/funciones.py
+++ Libros_web/funciones.py
@@ -11,8 +11,21 @@ def lee_argcivo_scv(archivo:str)->list:
         '''Crea un diccionario con los titulos como claves y ek resti de datos como valores'''
         return {x['titulo']:x for x in lista}
     
+    def buscar_en_titulo(diccionario, palabra)->list:
+       
+       lista = []
+       for titulo, libro in diccionario.items():
+           if palabra in titulo.lower():
+               lista.append(libro)
+    return lista
+
     if __name__ == "__main__":
      archivo_csv = 'booklist2000.csv'
      lista_libros = lee_archivo_csv(archivo_csv)
      diccionario_libros = crea_diccionario_titulos(lista_libros)
+     resultado = buscar_en_diccionario(diccionario_libros, 'rebels')
+     print(resultado)
+     diccionario_autores = crea_diccionario(lista_libros, 'autor')
+     resultado = buscar_en_diccionario(diccionario_autores, 'Sandra')
+     print(resultado)
     print(diccionario_libros)
